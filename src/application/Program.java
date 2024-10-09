261bab353064d3a7f52d7dbeebf66f37ae77d40b
AugustoRajao <augustocedro9@gmail.com>
2024-10-09 20:26:02 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -23,11 +23,16 @@ public class Program {
             System.out.println(obj);
         }
 
-        System.out.println("=== TEST 2: seller findAll ===");
+        System.out.println("=== TEST 3: seller findAll ===");
         sellerList = sellerDao.findAll();
         for(Seller obj : sellerList){
             System.out.println(obj);
         }
+
+        System.out.println("=== TEST 4: seller insert ===");
+        Seller newSeller = new Seller(null,"Greg","greg@gmail.com",new Date(),4000.0,department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
 
     }
 }
